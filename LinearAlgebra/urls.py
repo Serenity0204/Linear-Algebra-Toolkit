922974b6239eea23d82a88340e2d8b14bf855233
Yu-Heng Lin <yuhenglin0204@gmail.com>
2023-06-30 22:45:43 -0700

    changed to base.html and able to send matrix to backend

--- LinearAlgebra/urls.py
+++ LinearAlgebra/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("rref/", views.RREF_view, name="rref"),
 ]
--- LinearAlgebra/views.py
+++ LinearAlgebra/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+import json
 
 
 def index(request):
     return render(request, "index.html")
+
+
+def RREF_view(request):
+    if request.method == "POST":
+        matrix_data = request.POST.get("matrixData", "[]")
+        matrix = json.loads(matrix_data)
+
+        context = {"matrix": matrix}
+        return render(request, "rref.html", context)
